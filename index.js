080dff321cd7a2ba0442b6b9a1964dacda99e180
Talisa Foster <80614959+Talisa7@users.noreply.github.com>
2021-05-16 23:05:43 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function getNoteId() {
+    let noteObject= getExistingNotes()
+    if (!noteObject){
+        return 1
+    }
+
+    const keysArray = Object.keys(noteObject);
+    const numberkeys = keysArray.map((key)=> Number(key))
+    console.log (numberkeys);
+    return Math.max (... numberkeys) + 1;
+}
+
+function getExistingNotes() {
+    let notes = localStorage.getItem ('notes')
+    if (!notes){
+        return null
+    }
+    return JSON.parse (notes);
+}
